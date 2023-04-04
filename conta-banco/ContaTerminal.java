f24e3d8d6d8dac6b06d1c7bbbdba9574fb695603
SouzaBB <brunobsouzaa@gmail.com>
2023-04-04 13:15:28 -0300

    added try/catch

--- conta-banco/ContaTerminal.java
+++ conta-banco/ContaTerminal.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class ContaTerminal {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    try (Scanner scanner = new Scanner(System.in)) {
+      int numeroConta;
+      String agencia;
+      String nomeCliente;
 
-    int numeroConta;
-    String agencia;
-    String nomeCliente;
+      double saldo = 237.48;
 
-    double saldo = 237.48;
+      System.out.print("Digite seu nome: ");
+      nomeCliente = scanner.next();
 
-    System.out.print("Digite seu nome: ");
-    nomeCliente = scanner.next();
+      System.out.print("Digite sua agência: ");
+      agencia = scanner.next();
 
-    System.out.print("Digite sua agência: ");
-    agencia = scanner.next();
+      System.out.print("Digite o numero de sua conta: ");
+      numeroConta = scanner.nextInt();
 
-    System.out.print("Digite o numero de sua conta: ");
-    numeroConta = scanner.nextInt();
-
-    System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque");
+      System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque");
+    }
   
   }
 }
--- conta-banco/MovimentaConta.java
+++ conta-banco/MovimentaConta.java
@@ -1,7 +1,9 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class MovimentaConta {
+  
   public static void main(String[] args) {
     
     /* 
@@ -9,59 +11,61 @@ public class MovimentaConta {
     Esse parâmetro é importante no caso de realizar operações com double
     */
 
-    // Scanner lê o input do usuário
-    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+    try {// Scanner lê o input do usuário
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US); {
+      int saque;
+      int deposito;
+      int movimentação;
+      double saldo = 1000.00;
+      double limite = 200.00;
+      double limiteUtilizado;
+      double limiteEstourado;
 
-    double saque;
-    double deposito;
-    int movimentação;
-    double saldo = 1000.00;
-    double limite = 200.00;
-    double limiteUtilizado;
-    double limiteEstourado;
+      // selecionar opção: saque, depósito ou ver saldo
+      System.out.print("Digite a opção desejada: \n\n1 - Saque \n2 - Depósito \n3 - Saldo \n\nOpção: ");
+      movimentação = scan.nextInt();
 
-    // selecionar opção: saque, depósito ou ver saldo
-    System.out.print("Selecione a opção desejada: Saque [1], Depósito [2] ou Saldo [3]: ");
-    movimentação = scan.nextInt();
+      switch (movimentação) {
+        case 1:
+        System.out.print("Digite o valor de saque desejado: R$");
+        saque = scan.nextInt();
+          if (saque <= saldo) {
+            saldo = saldo - saque;
+            System.out.print("Seu novo saldo é de R$" + saldo);
 
-    switch (movimentação) {
-      case 1:
-      System.out.print("Digite o valor de saque desejado: ");
-      saque = scan.nextDouble();
-        if (saque <= saldo) {
-          saldo = saldo - saque;
-          System.out.print("Seu novo saldo é de R$" + saldo);
+          } else if (saque > saldo && saque < saldo + limite) {
+            limiteUtilizado = limite + (saldo - saque);
+            limite = limite - limiteUtilizado;
+            System.out.println("Você está utilizando R$" + limiteUtilizado + " do seu limite, seu limite atual é de R$" + limite);
+          
+          } else if (saque > saldo + limite) {
+            limiteEstourado = saque - (saldo + limite);
+            System.out.println("Você estourou seu limite em R$" + limiteEstourado);
+          }
+        break;
 
-        } else if (saque > saldo && saque < saldo + limite) {
-          limiteUtilizado = limite + (saldo - saque);
-          limite = limite - limiteUtilizado;
-          System.out.println("Você está utilizando R$" + limiteUtilizado + " do seu limite, seu limite atual é de R$" + limite);
-        
-        } else if (saque > saldo + limite) {
-          limiteEstourado = saque - (saldo + limite);
-          System.out.println("Você estourou seu limite em R$" + limiteEstourado);
+        case 2:
+        System.out.print("Digite o valor do depósito desejado: R$");
+        deposito = scan.nextInt();
+        saldo = saldo + deposito;
+        System.out.print("Seu novo saldo é de R$" + saldo);
+        break;
+
+        case 3:
+        System.out.print("Seu saldo é de R$" + saldo + 
+        ". Você possui limite disponível de R$" + limite);
+        break;
+
+        default:
+        System.out.println("Digite uma opção válida");
+        break;
         }
-      break;
-
-      case 2:
-      System.out.print("Digite o valor do depósito desejado: ");
-      deposito = scan.nextDouble();
-      saldo = saldo + deposito;
-      System.out.print("Seu novo saldo é de R$" + saldo);
-      break;
-
-      case 3:
-      System.out.print("Seu saldo é de R$" + saldo + 
-      ". Você possui limite disponível de R$" + limite);
-      break;
-
-      default:
-      System.out.println("Digite uma opção válida");
-
-    }
+      } scan.close();  
+    } catch ( InputMismatchException e) {
+      System.err.println("\n >>>> Erro: Valor de saque deve ser inteiro <<<<< \n");
+    }  
   }
-}      
-      
+}        
     /*
     Usando as condições 'if' e 'else if' para as opções de entrada
     Nessa caso, a variável 'movimentação' deve ser uma String
