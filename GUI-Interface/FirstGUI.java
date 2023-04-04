dffe6139f49a328a2d2416632f21e26f495b10c3
SouzaBB <brunobsouzaa@gmail.com>
2023-04-04 20:37:40 -0300

    First GUI

--- GUI-Interface/FirstGUI.java
+++ GUI-Interface/FirstGUI.java
@@ -1,21 +1,53 @@
+/******************************************************************************
+ *  Compilation:  javac GUI.java
+ *  Execution:    java GUI
+ *
+ *  A minimal Java program with a graphical user interface. The
+ *  GUI prints out the number of times the user clicks a button.
+ *
+ *  % java GUI
+ *
+ * Taken from: https://introcs.cs.princeton.edu/java/15inout/GUI.java.html
+ ******************************************************************************/
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
+public class FirstGUI implements ActionListener {
+    private int clicks = 0;
+    private JFrame frame = new JFrame();
+    private JLabel label = new JLabel("Number of clicks: 0");
 
-class FirstGUI {
-    public static void main(String args[]){
-        
-        JFrame frame = new JFrame("My First GUI");
+    public FirstGUI() {
+
+        // the clickable button
+        JButton button = new JButton("Click Me");
+        button.addActionListener(this);
+
+        // the panel with the button and text
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        // set up the frame and display it
+        frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        
-        JButton pressButton = new JButton("Press");
-        pressButton.setPreferredSize(new Dimension(100,100));
-
-        frame.add(pressButton);
-        frame.getContentPane().add(pressButton);
+        frame.setTitle("GUI");
+        frame.pack();
         frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
+    }
+    
+    // process the button clicks
+    public void actionPerformed(ActionEvent e) {
+        clicks++;
+        label.setText("Number of clicks: " + clicks);
+    }
+
+    // create one Frame
+    public static void main(String[] args) {
+        new FirstGUI();
     }
 }
