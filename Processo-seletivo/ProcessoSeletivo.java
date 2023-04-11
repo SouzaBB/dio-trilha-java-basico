c441e638b0e36123740b56b0c5dd0bd9f5c53107
SouzaBB <brunobsouzaa@gmail.com>
2023-04-11 10:33:31 -0300

    code completed

--- Processo-seletivo/ProcessoSeletivo.java
+++ Processo-seletivo/ProcessoSeletivo.java
@@ -1,13 +1,59 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    selecaoCandidatos();
+    String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+    for (String candidato: candidatos) {
+      entrandoEmContato(candidato);
+    }
+  }
+  static void entrandoEmContato(String candidato) {
+    int tentativasRealizadas = 1;
+    boolean continuarTentando = true;
+    boolean atendeu = false;
+    do {
+      atendeu = atender();
+      continuarTentando = !atendeu;
+      if (continuarTentando)
+        tentativasRealizadas++;
+      else
+        System.out.println("Contato realizado com sucesso!");
+
+    } while (continuarTentando && tentativasRealizadas < 3);
+
+    if (atendeu)
+      System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+    else
+      System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas " + tentativasRealizadas);
+
+  }
+
+  // Método auxiliar
+  static boolean atender() {
+    return new Random().nextInt(3) == 1;
+  }
+
+  static void imprimirSelecionados() {
+    String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+
+    System.out.println("\nImprimindo a lista de candidatos informando o indice do elemento\n");
+
+    for (int indice = 0; indice < candidatos.length; indice++) {
+      System.out.println("O candidato de número " + (indice+1) + " é o " + candidatos[indice]);
+    }
+
+    // Não irá imprimir o índice
+
+    System.out.println("\nForma abreviada de interação for each\n");
+
+    for (String candidato: candidatos) {
+      System.out.println("O candidato selecionado foi " + candidato);
+    }
   }
 
   static void selecaoCandidatos() {
-    String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO",
-                            "MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
+    String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
 
     int candidatosSelecionados = 0;
     int candidatoAtual = 0;
@@ -17,7 +63,7 @@ public class ProcessoSeletivo {
       String candidato = candidatos[candidatoAtual];
       double salarioPretendido = valorPretendido();
 
-      System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+      System.out.println("O candidato " + candidato + " Solicitou este valor de salário: " + salarioPretendido);
       if (salarioBase >= salarioPretendido) {
         System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
         candidatosSelecionados++;
